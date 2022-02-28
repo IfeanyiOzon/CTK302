21a3bc859d140f427a616deb14a96a6f5dd4f9d5
IfeanyiOzon <iozonwa@ilstu.edu>
2022-02-27 20:22:32 -0600

    new

--- Unit1/fonts/sketch.js
+++ Unit1/fonts/sketch.js
@@ -2,36 +2,23 @@ let rowdiesR;
 let syneR;
 
 
-<<<<<<< HEAD
-=======
-
-
->>>>>>> 881474ebacb7b1b31987a356c33e1684c426a279
 
 function setup() {
   createCanvas(500, 500);
-  rowdiesR = loadFont("assets/Rowdies-R.ttf") ;
-  syneR = loadFont("assets/SyneTactile-R.ttf") ;
+  rowdiesR = loadFont("assets/Rowdies-R.ttf");
+  syneR = loadFont("assets/SyneTactile-R.ttf");
 
 }
 
 function draw() {
-  background(100) ;
-<<<<<<< HEAD
+  background(100);
 
   textFont(rowdiesR);
   textSize(48);
-  text("it's your boy",100 ,100);
+  text("it's your boy", 100, 100);
 
-  textFont(syneR) ;
-  textSize(48) ;
-=======
-  textFont(rowdiesr);
-  textSize(48)
-text("it's your boy", 100, 100) ;
-  textFont(syner);
-  textSize(48)
->>>>>>> 881474ebacb7b1b31987a356c33e1684c426a279
-  text("it's your boy", 200, 300) ;
+  textFont(syneR);
+  textSize(48);
+  text("it's your girl", 100, 300);
 
 }
